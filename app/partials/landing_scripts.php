fc516d37da0b4b4e065ef19403c954b24e9cdb60
Martin Mbithi <martdevelopers254@gmail.com>
2022-08-18 08:54:27 +0300

    Success Intergration And Segmentation Of Landing Page

---
+++ app/partials/landing_scripts.php
@@ -0,0 +1,32 @@
+<script src="../public/landing_assets/js/vendor/jquery-3.5.1.min.js"></script>
+<script src="../public/landing_assets/js/vendor/jquery.notify.min.js"></script>
+<script src="../public/landing_assets/js/vendor/jquery.bundle.notify.min.js"></script>
+
+<script src="../public/landing_assets/js/vendor/popper.min.js"></script>
+<script src="../public/landing_assets/js/vendor/bootstrap.min.js"></script>
+<script src="../public/landing_assets/js/vendor/jquery-migrate-3.3.0.min.js"></script>
+<script src="../public/landing_assets/js/vendor/modernizr-3.11.2.min.js"></script>
+
+<!--Plugins JS-->
+<script src="../public/landing_assets/js/plugins/swiper-bundle.min.js"></script>
+<script src="../public/landing_assets/js/plugins/countdownTimer.min.js"></script>
+<script src="../public/landing_assets/js/plugins/scrollup.js"></script>
+<script src="../public/landing_assets/js/plugins/jquery.zoom.min.js"></script>
+<script src="../public/landing_assets/js/plugins/slick.min.js"></script>
+<script src="../public/landing_assets/js/plugins/infiniteslidev2.js"></script>
+<script src="../public/landing_assets/js/vendor/jquery.magnific-popup.min.js"></script>
+<script src="../public/landing_assets/js/plugins/jquery.sticky-sidebar.js"></script>
+<!-- Google translate Js -->
+<script src="../public/landing_assets/js/vendor/google-translate.js"></script>
+<script>
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+                pageLanguage: "en"
+            },
+            "google_translate_element"
+        );
+    }
+</script>
+<!-- Main Js -->
+<script src="../public/landing_assets/js/vendor/index.js"></script>
+<script src="../public/landing_assets/js/main.js"></script>
