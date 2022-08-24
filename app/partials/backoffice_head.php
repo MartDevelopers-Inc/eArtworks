f6e44300fbf3084d71192255b6408602a0831fc5
Martin Mbithi <martdevelopers254@gmail.com>
2022-08-24 12:12:38 +0300

    Intergrate Back Office Header Assets

--- app/partials/backoffice_head.php
+++ app/partials/backoffice_head.php
@@ -7,23 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="Ekka - Admin Dashboard eCommerce HTML Template.">
 
-    <title>Ekka - Admin Dashboard eCommerce HTML Template.</title>
+    <title>eArtstore - The Ultimate Artworks Online Store.</title>
 
     <!-- GOOGLE FONTS -->
-    <link rel="preconnect" href="../../../../fonts.googleapis.com/index.html">
-    <link rel="preconnect" href="../../../../fonts.gstatic.com/index.html" crossorigin>
-    <link href="../../../../fonts.googleapis.com/css21b01.css?family=Montserrat:wght@200;300;400;500;600;700;800&amp;family=Poppins:wght@300;400;500;600;700;800;900&amp;family=Roboto:wght@400;500;700;900&amp;display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800&family=Poppins:wght@300;400;500;600;700;800;900&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
 
-    <link href="../../../../cdn.jsdelivr.net/npm/%40mdi/font%404.4.95/css/materialdesignicons.min.css" rel="stylesheet" />
+    <link href="https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css" rel="stylesheet" />
 
     <!-- PLUGINS CSS STYLE -->
-    <link href="assets/plugins/daterangepicker/daterangepicker.css" rel="stylesheet">
-    <link href="assets/plugins/simplebar/simplebar.css" rel="stylesheet" />
+    <link href="../public/backoffice_assets/plugins/daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="../public/backoffice_assets/plugins/simplebar/simplebar.css" rel="stylesheet" />
 
     <!-- Ekka CSS -->
-    <link id="ekka-css" href="assets/css/ekka.css" rel="stylesheet" />
+    <link id="ekka-css" href="../public/backoffice_assets/css/ekka.css" rel="stylesheet" />
 
     <!-- FAVICON -->
-    <link href="assets/img/favicon.png" rel="shortcut icon" />
+    <link href="../public/backoffice_assets/img/favicon.png" rel="shortcut icon" />
 
 </head>
