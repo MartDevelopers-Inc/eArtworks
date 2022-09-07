9288397b7072f7f0e6b24b07331ee86da9e639ec
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-07 13:22:55 +0300

    Implement Shopping Cart Logic

--- ui/landing_product.php
+++ ui/landing_product.php
@@ -71,6 +71,52 @@ require_once('../app/settings/checklogin.php');
 checklogin();
 require_once('../app/helpers/landing.php');
 $db_handler = new DBController();
+if (!empty($_GET["action"])) {
+    switch ($_GET["action"]) {
+        case "add":
+            if (!empty($_POST["quantity"])) {
+                $productByCode = $db_handler->runQuery("SELECT * FROM products WHERE product_sku_code='" . $_GET["SKU"] . "'");
+                $itemArray = array($productByCode[0]["SKU"] => array(
+                    'product_name' => $productByCode[0]["product_name"],
+                    'product_sku_code' => $productByCode[0]["product_sku_code"],
+                    'quantity' => $_POST["quantity"],
+                    'product_price' => $productByCode[0]["product_price"],
+                    'product_image' => $productByCode[0]["product_image"]
+                ));
+
+                if (!empty($_SESSION["cart_item"])) {
+                    if (in_array($productByCode[0]["product_sku_code"], array_keys($_SESSION["cart_item"]))) {
+                        foreach ($_SESSION["cart_item"] as $k => $v) {
+                            if ($productByCode[0]["product_sku_code"] == $k) {
+                                if (empty($_SESSION["cart_item"][$k]["quantity"])) {
+                                    $_SESSION["cart_item"][$k]["quantity"] = 0;
+                                }
+                                $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+                            }
+                        }
+                    } else {
+                        $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $itemArray);
+                    }
+                } else {
+                    $_SESSION["cart_item"] = $itemArray;
+                }
+            }
+            break;
+        case "remove":
+            if (!empty($_SESSION["cart_item"])) {
+                foreach ($_SESSION["cart_item"] as $k => $v) {
+                    if ($_GET["product_sku_code"] == $k)
+                        unset($_SESSION["cart_item"][$k]);
+                    if (empty($_SESSION["cart_item"]))
+                        unset($_SESSION["cart_item"]);
+                }
+            }
+            break;
+        case "empty":
+            unset($_SESSION["cart_item"]);
+            break;
+    }
+}
 require_once('../app/partials/landing_head.php');
 ?>
 
