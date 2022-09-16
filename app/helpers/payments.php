6c0be6839c8f2e3d5dfd880102cad190a6d347b8
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-16 14:49:50 +0300

    Update MPESA Payment Logic - Implement C2B Via Lipa Na Mpesa

--- app/helpers/payments.php
+++ app/helpers/payments.php
@@ -145,6 +145,98 @@ if (isset($_POST['Add_Payment'])) {
         } else {
             $err =  'We can not process your payment';
         }
+    } else if ($payment_method_name == 'Mobile Payment') {
+        /* Persist Payment Record But Without Payment Code */
+        $sql = "INSERT INTO payments (payment_order_code, payment_means_id, payment_amount) 
+        VALUES('{$payment_order_code}', '{$payment_means_id}', '{$payment_amount}')";
+
+        /* Load Mpesa STK PUSH */
+        date_default_timezone_set('Africa/Nairobi');
+
+        # access token
+        $consumerKey = ''; //Fill with your app Consumer Key
+        $consumerSecret = ''; // Fill with your app Secret
+
+        # define the variales
+        # provide the following details, this part is found on your test credentials on the developer account
+        $Amount = '';
+        $BusinessShortCode = '';
+        $Passkey = '';
+
+        /*
+            This are your info, for
+            $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
+            $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
+            TransactionDesc can be anything, probably a better description of or the transaction
+            $Amount this is the total invoiced amount, Any amount here will be 
+            actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
+            for developer/test accounts, this money will be reversed automatically by midnight.
+        */
+
+        $PartyA =  $user_contacts;
+        $AccountReference = 'eArtworks';
+        $TransactionDesc = 'Mobile Payment for order number ' . $payment_order_code;
+
+        # Get the timestamp, format YYYYmmddhms -> 20181004151020
+        $Timestamp = date('YmdHis');
+
+        # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+        $Password = base64_encode($BusinessShortCode . $Passkey . $Timestamp);
+
+        # header for access token
+        $headers = ['Content-Type:application/json; charset=utf8'];
+
+        # M-PESA endpoint urls
+        $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+        $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+        # callback url
+        $CallBackURL = '/eArtworks/ui/callback_url.php?order=' . $payment_order_code;
+
+        $curl = curl_init($access_token_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_HEADER, FALSE);
+        curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+        $result = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $result = json_decode($result);
+        $access_token = $result->access_token;
+        curl_close($curl);
+
+        # header for stk push
+        $stkheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
+
+        # initiating the transaction
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $initiate_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+
+        $curl_post_data = array(
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => $BusinessShortCode,
+            'Password' => $Password,
+            'Timestamp' => $Timestamp,
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => $Amount,
+            'PartyA' => $PartyA,
+            'PartyB' => $BusinessShortCode,
+            'PhoneNumber' => $PartyA,
+            'CallBackURL' => $CallBackURL,
+            'AccountReference' => $AccountReference,
+            'TransactionDesc' => $TransactionDesc
+        );
+
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
+        if (mysqli_query($mysqli, $sql)) {
+            $success = "Order $payment_order_code paid";
+        } else {
+            $err = "Failed, please try again";
+        }
     } else {
         $err = "Payment means is not supported yet";
     }
